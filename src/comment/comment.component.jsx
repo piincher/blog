329b5c1fbed2e8b1e4fe6028c89643e2e053b853
Pro <pro@Pros-MacBook-Pro.local>
2020-09-14 05:54:08 +0800

    add approuve button and reject button

--- src/comment/comment.component.jsx
+++ src/comment/comment.component.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import faker from 'faker'
 
+const Comment = ({ author, timeAgo, text, avatar }) => (
+	<div className="comment">
+		<a href="/" className="avatar">
+			<img alt="avatar" src={avatar} />
+		</a>
+		<div className="content">
+			<a href="/" className="author">
+				{author}
+			</a>
+			<div className="metadata">
+				<span className="date">{timeAgo}</span>
+			</div>
+			<div className="text">{text}</div>
+		</div>
+	</div>
+);
 
-const Comment = ({name}) => (
-     <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        {name}
-                    </a>
-                    <div className="metadata">
-                        <span className="date">aujourdhui a 16h20mn</span>
-                    </div>
-                    <div className="text">
-                        nice blog post
-                    </div>
-                </div>
-            </div>
-)
-
-export default Comment
-
+export default Comment;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Comment from '../src/comment/comment.component'
+import Comment from '../src/comment/comment.component';
+import faker from 'faker';
+import ApprovalCard from '../src/approvalCard/approvalCard.component';
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <Comment name='piincher'/>
-            <Comment  name='lecze'/>
-            <Comment name='focxy'/>
-        </div>
-    )
-        
-    
-}
+	return (
+		<div className="ui container comments">
+			<ApprovalCard />
+			<Comment
+				author="piincher"
+				timeAgo="today at 4h:45mn"
+				text="interesting blog"
+				avatar={faker.image.avatar()}
+			/>
+			<Comment author="lecze" timeAgo="today at 5h:09mn" text="very cool post" avatar={faker.image.avatar()} />
+			<Comment author="focxy" timeAgo="today at 08h:07mn" text="nice blog" avatar={faker.image.avatar()} />
+		</div>
+	);
+};
 
-ReactDom.render(<App />, document.querySelector("#root"))
+ReactDom.render(<App />, document.querySelector('#root'));
